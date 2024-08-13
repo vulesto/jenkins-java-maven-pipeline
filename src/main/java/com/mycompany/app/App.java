b69f865b96dba4e6c9ad8598ed4897635f045209
Atul Kamble <atul_kamble@hotmail.com>
2024-08-13 14:41:43 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,19 +1,7 @@
-package com.mycompany.app;
+package com.example;
 
-/**
- * Hello world!
- */
 public class App {
-
-    private static final String MESSAGE = "Hello World!";
-
-    public App() {}
-
     public static void main(String[] args) {
-        System.out.println(MESSAGE);
-    }
-
-    public String getMessage() {
-        return MESSAGE;
+        System.out.println("Hello, World!");
     }
 }
